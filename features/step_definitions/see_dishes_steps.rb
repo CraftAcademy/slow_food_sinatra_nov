457b0823799732f51295e950cacd4ff4bc502432
kriizjones <kristoffer.s.karlsson@hotmail.com>
2016-11-30 12:43:40 +0100

    enter the first steps of our feature see_dishes

---
+++ features/step_definitions/see_dishes_steps.rb
@@ -0,0 +1,15 @@
+When(/^I visit the dishes page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I see "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I see the price of "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I see "([^"]*)" button$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
