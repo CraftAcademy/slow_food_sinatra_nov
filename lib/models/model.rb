adfafdd0affd25e55655f8f07e2ab06469fd4ace
rmzse <rodrigo@therawdelicious.com>
2016-11-30 11:21:19 +0100

    comment out dish model from models

--- lib/models/model.rb
+++ lib/models/model.rb
@@ -17,3 +17,14 @@ class User
 end
 
 
+# class Dish
+#   include DataMapper::Resource
+#
+#   property :id, Serial, key: true
+#   property :name, String, length: 100, :required => true
+#   property :price, Integer, :required => true
+#   property :category, String, length: 11, :required => true
+#
+#   belongs_to :orders, :key => true
+#   belongs_to :restaurant, :key => true
+# end
