a1b99c838eaaa5239db15ba1740a7dc3e9b7eb30
rmzse <rodrigo@therawdelicious.com>
2016-11-30 20:32:52 +0100

    edit dish name to be 100 chars and remove primary key from dish price property, both changes in the dish model

--- lib/models/dish.rb
+++ lib/models/dish.rb
@@ -2,8 +2,8 @@ class Dish
   include DataMapper::Resource
 
   property :id, Serial, key: true
-  property :name, String, length: 128
-  property :price, Float, key: true
+  property :name, String, length: 100
+  property :price, Float
 
 
 end
