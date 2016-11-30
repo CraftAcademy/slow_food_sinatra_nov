5068d988ab910a00bb66ef375a4d3d4e3297de6b
fredrikfalkman <ff@ffd.se>
2016-11-30 15:59:42 +0100

    adds test on order for amount and pickup_time properties + passing implementation

--- lib/models/order.rb
+++ lib/models/order.rb
@@ -2,5 +2,6 @@ class Order
   include DataMapper::Resource
 
   property :id, Serial, key: true
-
+  property :amount, Integer
+  property :pickup_time, Time
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,5 +2,7 @@ require './lib/models/order'
 
 describe Order do
   it{is_expected.to have_property :id}
+  it{is_expected.to have_property :amount}
+  it{is_expected.to have_property :pickup_time}
 
 end
